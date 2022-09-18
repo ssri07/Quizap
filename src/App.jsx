c5137db9a0408d6a61fa2a90b360270db8d20653
abdullmustyy <abdulmusty03@outlook.com>
2022-09-18 21:14:28 +0100

    Add useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { nanoid } from "nanoid";
 import Start from "./components/Start";
 import Quiz from "./components/Quiz";
@@ -18,29 +18,52 @@ export default function App() {
     getQuestions();
   }, []);
 
-  function startQuiz() {
+  const startQuiz = useCallback(() => {
     setQuizObject(
       quizData.map((result) => ({
         id: nanoid(),
         question: result.question,
-        correct: result.correct_answer,
-        wrong: result.incorrect_answers,
-        isPicked: false,
+        options: [
+          { id: nanoid(), isPicked: false, option: result.correct_answer },
+          {
+            id: nanoid(),
+            isPicked: false,
+            option: result.incorrect_answers[0],
+          },
+          {
+            id: nanoid(),
+            isPicked: false,
+            option: result.incorrect_answers[1],
+          },
+          {
+            id: nanoid(),
+            isPicked: false,
+            option: result.incorrect_answers[2],
+          },
+        ],
       }))
     );
-  }
+  }, [quizData]);
 
-  function toggleIsPicked(id) {
-    setQuizObject((prevQuizObject) =>
-      prevQuizObject.map((quiz) => {
-        quiz.id === id ? { ...quiz, isPicked: !quiz.isPicked } : quiz;
-      })
-    );
+  const toggleIsPicked = useCallback(
+    (id, option) => {
+      setQuizObject(
+        quizObject.map((object) => {
+          object.options.map((option, i) =>
+            option.id === id
+              ? {
+                  ...object,
+                  [object.options[i].isPicked]: !object.options[i].isPicked,
+                }
+              : object
+          );
+        })
+      );
 
-    // setQuizObject((prevQuiz) =>
-    //   prevQuiz.map((quiz) => ({ ...quiz, isPicked: !quiz.isPicked }))
-    // );
-  }
+      console.log(`Change your fucking color now! ${option}:${id}`);
+    },
+    [quizObject]
+  );
 
   return (
     <section className="app bg-[#eff2f9] text-[#293264] h-auto">
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,15 +1,20 @@
-export default function Questions({
-  question,
-  correct,
-  wrong,
-  no,
-  id,
-  pick,
-  isPicked,
-}) {
-  const styles = {
-    backgroundColor: isPicked ? "#D6DBF5" : "",
-  };
+export default function Questions({ question, options, no, id, pick }) {
+  const optionButtons = options.map((option, i) => {
+    const styles = {
+      backgroundColor: option.isPicked ? "#D6DBF5" : "",
+    };
+
+    return (
+      <button
+        key={option.id}
+        style={styles}
+        onClick={() => pick(option.id, option.option)}
+        className="font-inter font-medium text-xs leading-4 border border-solid border-[#4D5B9E] px-4 py-2 rounded-lg hover:bg-[#D6DBF5] hover:text-white focus:outline-none focus:ring focus:ring-[#4D5B9E]"
+      >
+        {option.option}
+      </button>
+    );
+  });
 
   return (
     <div className="flex flex-col space-y-4 py-4 px-8 border-b border-[#DBDEF0]">
@@ -18,34 +23,35 @@ export default function Questions({
         {no}). {question}{" "}
       </h1>
       <div className="flex space-x-3">
-        <button
+        {optionButtons}
+        {/* <button
           style={styles}
           onClick={() => pick(id)}
           className="font-inter font-medium text-xs leading-4 border border-solid border-[#4D5B9E] px-4 py-2 rounded-lg hover:bg-[#D6DBF5] hover:text-white focus:outline-none focus:ring focus:ring-[#4D5B9E]"
         >
-          {correct}
+          {options[0].option1}
         </button>
         <button
           style={styles}
           onClick={() => pick(id)}
           className="font-inter font-medium text-xs leading-4 border border-solid border-[#4D5B9E] px-4 py-2 rounded-lg hover:bg-[#D6DBF5] hover:text-white focus:outline-none focus:ring focus:ring-[#4D5B9E]"
         >
-          {wrong[0]}
+          {options[1].option2}
         </button>
         <button
           style={styles}
           onClick={() => pick(id)}
           className="font-inter font-medium text-xs leading-4 border border-solid border-[#4D5B9E] px-4 py-2 rounded-lg hover:bg-[#D6DBF5] hover:text-white focus:outline-none focus:ring focus:ring-[#4D5B9E]"
         >
-          {wrong[1]}
+          {options[2].option3}
         </button>
         <button
           style={styles}
           onClick={() => pick(id)}
           className="font-inter font-medium text-xs leading-4 border border-solid border-[#4D5B9E] px-4 py-2 rounded-lg hover:bg-[#D6DBF5] hover:text-white focus:outline-none focus:ring focus:ring-[#4D5B9E]"
         >
-          {wrong[2]}
-        </button>
+          {options[3].option4}
+        </button> */}
       </div>
     </div>
   );
